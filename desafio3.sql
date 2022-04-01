57c3de6ea737218ae98d58cfc4f10ef803cbae7f
Gabriel-Araujo11 <gabriel_arsi@hotmail.com>
2022-04-01 00:12:13 -0300

    fix(table): refatora requisito 3

--- desafio3.sql
+++ desafio3.sql
@@ -1,14 +1,13 @@
-SELECT usr.user_name AS usuario,  
+SELECT usrr.user_name AS usuario,  
+COUNT(historyy.user_id) AS qtde_musicas_ouvidas,
 
-COUNT(historyy.artist_id) AS qtde_musicas_ouvidas,
 ROUND(SUM(music.duration) / 60, 2) AS total_minutos
-
-FROM SpotifyClone.users AS usr
+FROM SpotifyClone.users AS usrr
 
 JOIN history_rep AS historyy
-ON usr.user_name = historyy.artist_id
+ON usrr.user_id = historyy.user_id
 
 JOIN songs AS music
-ON music.id_sond = historyy.song_id
+ON music.id_song = historyy.song_id
 
-GROUP BY usr.user_name ORDER BY usr.user_name;
+GROUP BY usrr.user_id ORDER BY usrr.user_name;
